95a9fbcb87714686f0c46466bde45ae0dd281470
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-20 20:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday,name) {
-  return `Happy ${holiday},${name}!`
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days,holiday){
+  return `It's ${days} days until ${holiday}!`
 }
